95072a8c621a2853d968106b1fb22bab9aad68da
su-60111 <su086010@qq.com>
2025-11-28 18:42:06 +0700

    change add

--- a.c
+++ a.c
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-	printf("tasting......|n");
+	printf("1tasting......|n");
+	printf("2tasting......|n");
+	printf("3tasting......|n");
+	printf("4tasting......|n");
+	printf("5tasting......|n");
+	printf("6tasting......|n");
+	
 
-	return 0;
+
+
+
 }
